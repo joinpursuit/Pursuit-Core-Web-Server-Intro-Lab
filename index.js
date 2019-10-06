63ddb8e91df77908e81368082d7ed7db55377925
jonathan <jonathanfagan@pursuit.org>
2019-10-06 18:02:17 -0400

    frame work started for creating random card

--- index.js
+++ index.js
@@ -14,7 +14,23 @@ async function loadDataFromServer() {
 
 function displayDataFromServer(data) {
     console.log(data);
+    // this is to create the outter card
+    let container = document.createElement('div')
+    container.setAttribute("class", "card")
+    //to create image tag
+    let image = document.createElement('img')
+    //let img = finish in a min
+    // image.setAttribute("src", /*img*/)
     
+    //this is for inner card
+    let innerContainer = document.createElement('div')
+    innerContainer.setAttribute("class", "container")
+    let header1 = document.createElement("h4")
+    //header1.innerText = finish later
+    let ptag = document.createElement('p')
+    //ptag.innerText = finish later
+
+    //append stuff in order after your done
     
     
 }
