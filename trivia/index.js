8b75e701451d2425c897512c86e1473c54732b0f
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-10-06 22:55:45 -0400

    Added a console log where the final answer will be checked

--- trivia/index.js
+++ trivia/index.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () =>{
     fillSelectTag();
+
+    let thisForm = document.querySelector('#form')
+
+    thisForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        checkAnswer();
+    })
     // loadDataFromServer();
-    // getServerButton().addEventListener('click', loadDataFromServer)
+    // getServerButton().addEventListener('click', checkAnswer)
 })
 
 async function fillSelectTag () {
@@ -11,9 +18,12 @@ async function fillSelectTag () {
 }
 
 function getServerButton() {
-    return document.querySelector("#serverButton")
+    return document.querySelector("#submit-answer")
 }
 
+function checkAnswer () {
+        console.log('Getting There')
+}
 // async function loadDataFromServer() {
 //     const myURL = "http://localhost:3000/"
 //     const resp = await axios.get(myURL)    
@@ -67,51 +77,6 @@ function displayDataFromServer(data) {
                 questionSelection.appendChild(guessAnswer1);
                 questionSelection.appendChild(guessAnswer2);
             }
-
-
         }
-
     }
-
-
-    // for(let i = 0; i < data.results.length; i++) {
-    // let triviaHolder = document.createElement('div');
-    // triviaHolder.id = 'trivia';
-
-    // const newCard = document.createElement('img');
-
-    // if(data.results[i].gender === 'male') {
-    //     newCard.src = "img_avatar.png"
-    // } else {
-    //     newCard.src = "img_avatar2.png"
-    // }
-
-    // newCard.alt = "Avatar";
-    // newCard.style = "width:100%";
-
-    // cardHolder.appendChild(newCard);
-
-    // let currQuestion = document.createElement('p');
-    // let possAnswers = document.createElement('select');
-    // let userAnswer = document.createElement('button');
-
-    // currQuestion.id = 'question';
-    // possAnswers.id = 'answers';
-    
-
-
-    // const fullName = document.createElement('p');
-    // fullName.innerText = `${data.results[i].name.first.toUpperCase()} ${data.results[i].name.last.toUpperCase()}`
-    // fullName.style.fontWeight = 'bold';
-
-    // const userTitle = document.createElement('p');
-    // userTitle.innerText = `${data.results[i].name.title}`
-
-    // userHolder.appendChild(fullName)
-    // userHolder.appendChild(userTitle)
-    // cardHolder.appendChild(userHolder);
-
-    // document.body.appendChild(cardHolder);
-
-    // }
 }
