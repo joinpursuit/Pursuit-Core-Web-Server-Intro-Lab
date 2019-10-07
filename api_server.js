f58323d9b119de8e5c8ed20c6d4a444288c62360
jenama <johanneenama@gmail.com>
2019-10-07 00:01:40 -0400

    still working on it

--- api_server.js
+++ api_server.js
@@ -1,39 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-    grabServerButton().addEventListener("click", loadData)
-}) 
+document.addEventListener("DOMContentLoaded", () => { //loading the DOM and listening for the grabUserButton function to 
+    grabUserButton().addEventListener("click", loadData)
+})
 
-const grabServerButton = () => {
-    return document.querySelector("#serverButton")
+const grabUserButton = () => {
+    return document.querySelector("#getUser") // getting the button by its id
 }
 
 const loadData = async () => {
     const url = "https://randomuser.me/api/?inc=gender,name,nat&&results=10"
-    let response = await axios.get(url)
-    getRandom()
-    displayData(response.data)
-     console.log(response.data)
-}
-
-const getRandom = () => {
-    return Math.random() * (10- 1) + 1;
+    let response = await axios.get(url) // fetching the data with the list of users and the required info.
+    displayData(response.data.results)
     
+
 }
 
-const displayData = (data) => {
-    const titleHeader = document.createElement('h1')
-    titleHeader.innerText = data.title;
+const displayData = (data) => { //displaying the data that we requested from the server. 
+        
+        let mainDiv = document.querySelector(".main-div")
+        
+        for (let i = 0; i < data.length; i++) {
 
-    const firstName = document.createElement("h2")
-    firstName.innerText = data.first;
+            let userTitle = document.querySelector("#title")
+            userTitle.innertext = data.title;
+            
+            let userFirstname = document.querySelector("#first")
+            userFirstname.innertext = data.first;
 
-    const lastName = document.createElement('h3')
-    lastName.innerText = data.last;
+            let userLastname = document.querySelector("#last")
+            userLastname.innertext = data.last;
 
-    const genderP = document.createElement('p')
-    genderP.innerText = data.gender;
+            let userGender = document.querySelector("#gender")
+            userGender.innertext = data.gender;
 
-    const nat = document.createElement("p")
-    nat.innerText = data.nationality
+            let userNationality = document.querySelector("#nationality")
+            userNationality.innertext = data.nationality;
 
-    document.body.append(titleHeader, lastName, firstName, genderP, nat)
+            mainDiv.append(userTitle, userFirstname, userLastname, userGender, userNationality)
+
+        }
 }
