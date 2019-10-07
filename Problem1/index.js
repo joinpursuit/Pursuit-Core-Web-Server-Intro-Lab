6844ccf1af7bcab0d6baf7a7e0a8e41ed2e01db2
DejaLFlynn <dejaflynn@pursuit.org>
2019-10-07 18:55:14 -0400

    pictureable to switch

--- Problem1/index.js
+++ Problem1/index.js
@@ -9,10 +9,9 @@ function getServerButton() {
 async function loadDataFromServer() {
     const myURL = "http://localhost:3000"
     const resp = await axios.get(myURL) 
-    let main = document.querySelector('body')
-    let button= document.querySelector('button');
-    button.style.display = 'none'
-    displayResponseFromServer(resp.data)
+
+    // displayResponseFromServer(resp.data)
+    randomServerData(resp.data)
 }
 function displayResponseFromServer(data) {
     let serverResults = data.results
@@ -21,28 +20,34 @@ function displayResponseFromServer(data) {
 
 function randomServerData(data) {
     let randomNum = Math.floor(Math.random() * data.results.length)
+    let user = data;
+    let innerContainer = document.createElement('div');
+    document.body.appendChild(innerContainer);
+    innerContainer.setAttribute('class', 'card')
+    
+  console.log('error?')
+    if (data.results[randomNum].gender === 'male') {
+        let blueImage= document.createElement('img')
+        blueImage.src = "https://pedicon2020.com/img/avatar.png";
+         innerContainer.append(blueImage)
+     } else {
+         let pinkImg = document.createElement('img')
+         pinkImg.src = "https://www.showyourtrade.com/images/avatar/img_avatar.png";
+         innerContainer.append(pinkImg)
+ 
+        //  main.appendChild(innerContainer)
+ 
+     }
 
-
-    let innerContainer = document.createElement('div')
-    innerContainer.setAttribute('class', 'container')
+    
 
     let userNames = document.createElement('h4')
+
     let userFirstLast = `${data.results[randomNum].name.first} ${data.results[randomNum].name.last}`;
-    userNames.innerText = userFirstLast
+    userNames.innerText = `${user.results[randomNum].name.first} ${user.results[randomNum].name.last}`;
 
     let paragraph = document.createElement('p')
-    paragraph.innerText = `Nationality: ${response.data.nat}`
-    document.body.append(emptyDiv, profilePic, innerContainer, userFirstLast, paragraph)
-    if (data.results[randomNum].gender === 'male') {
-       let blueImage= document.createElement('img')
-       blueImage.src = "https://pedicon2020.com/img/avatar.png";
-        innerContainer.append(blueImage)
-    } else {
-        let pinkImg = document.createElement('img')
-        pinkImg.src = "https://www.showyourtrade.com/images/avatar/img_avatar.png";
-        innerContainer.append(pinkImg)
+    paragraph.innerText = `Nationality: ${data.nat}`
+    document.body.appendChild(innerContainer)
 
-        main.appendChild(innerContainer)
-
-    }
 }
