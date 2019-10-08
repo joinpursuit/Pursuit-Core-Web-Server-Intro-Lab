20af99a8ca5c65f65bb61c93a22f4d64457e9d8c
karenm09 <karenm929@hotmail.com>
2019-10-07 20:05:17 -0400

    better working on the print

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     getServerButton().addEventListener('click', loadDataFromServer)
 })
 
@@ -8,10 +8,40 @@ const getServerButton = () => {
 
 async function loadDataFromServer() {
     const myURL = "http://localhost:3000/"
-    const resp = await axios.get(myURL)    
+    const resp = await axios.get(myURL)
     displayDataFromServer(resp.data)
 }
-
 const displayDataFromServer = (data) => {
+    let randomUser = Math.floor(Math.random() * 10)
     console.log(data);
+    
+    let profilePic = document.createElement('img')
+    const girlPicUrl = './blackGirl.png'
+    const boyPicUrl = './blackGuy.jpeg'
+
+    const profileContainer = document.createElement("div");
+    profileContainer.setAttribute("class", "card");
+
+    const profileContent = document.createElement("div");
+    profileContent.setAttribute("class", "profile-data");
+
+    
+    let fullName = document.createElement('p')
+    fullName.innerText = ""
+    // profileContent.innerText = '';
+    if (data.results[randomUser].gender === 'male') {
+        profileContent.innerText = '';
+        profilePic.src = boyPicUrl;
+        fullName.innerText = `${data.results[randomUser].name.title} ${data.results[randomUser].name.first} ${data.results[randomUser].name.last}`;
+    } else {
+        profileContent.innerText = '';
+        let profilePic = document.createElement('img')
+        profilePic.src = girlPicUrl;
+        fullName.innerText = `${data.results[randomUser].name.title} ${data.results[randomUser].name.first} ${data.results[randomUser].name.last}`;
+    }
+    //document.body.appendChild(profileContent)
+    document.body.appendChild(profileContainer)
+    profileContent.appendChild(fullName)
+    profileContainer.appendChild(profilePic) 
+    // profileContainer.appendChild(profileContent)
 }
