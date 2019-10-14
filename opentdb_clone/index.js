07039687959d3bbb1aa19d96d0bccde6f0f20a7c
Hupaul <hupaulcamacho@pursuit.org>
2019-10-13 22:58:17 -0400

    add element creators within a function, display trivia question to webpage

--- opentdb_clone/index.js
+++ opentdb_clone/index.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', () =>{
-    getServerButton().addEventListener('click', loadDataFromServer)
+    let button = document.querySelector("#serverButton")
+    button.addEventListener('click', generateTrivia)
 })
 
-function getServerButton() {
-    return document.querySelector("#serverButton")
-}
-
-async function loadDataFromServer() {
+const generateTrivia = async () => {
+    let trivia = document.createElement("p");
+    let select = document.createElement("select")
+    let option = document.createElement("option")
     const myURL = "http://localhost:3000/"
-    const response = await axios.get(myURL)    
-    displayDataFromServer(response.data)
+    const response = await axios.get(myURL)   
+    let triviaSelection = response.data.results[getRandomNum()]
+    // console.log(response.data.results[getRandomNum()])
+    // console.log(question)
+
+    trivia.innerText = `Question: ${triviaSelection.question}`
+    console.log(response.data)
+     for(let answer of response.data.results)
+    document.body.append(trivia)
+    document.body.append(select)
 }
 
 const getRandomNum = () => {
     return Math.floor((Math.random() * 10) + 1);
 }
 
-function displayDataFromServer(data) {
-    console.log(data)
-}
