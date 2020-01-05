713b40271816b7dc8607759417cde8add2fb36a7
ashya <ashyamanning@pursuit.org>
2020-01-05 16:39:18 -0500

    build server

--- app.js
+++ app.js
@@ -1,103 +1,116 @@
 const http = require("http");
 const port =  3000;
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");  
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");  
     
-    const randomUsers = [
+  const data = {
+      "results": [
         {
           "gender": "female",
           "name": {
-            "title": "mademoiselle",
-            "first": "ruth",
-            "last": "nicolas"
+            "title": "Mrs",
+            "first": "Véronique",
+            "last": "Van Oirschot"
           },
-          "nat": "CH"
+          "nat": "NL"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "Mr",
+            "first": "Troy",
+            "last": "Stevens"
+          },
+          "nat": "IE"
         },
         {
           "gender": "female",
           "name": {
-            "title": "miss",
-            "first": "رها",
-            "last": "سلطانی نژاد"
+            "title": "Ms",
+            "first": "Lucie",
+            "last": "Nguyen"
+          },
+          "nat": "FR"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "Mr",
+            "first": "Agnelo",
+            "last": "da Costa"
+          },
+          "nat": "BR"
+        },
+        {
+          "gender": "female",
+          "name": {
+            "title": "Ms",
+            "first": "Sofia",
+            "last": "Lo"
+          },
+          "nat": "CA"
+        },
+        {
+          "gender": "male",
+          "name": {
+            "title": "Mr",
+            "first": "حسین",
+            "last": "رضایی"
           },
           "nat": "IR"
         },
         {
           "gender": "female",
           "name": {
-            "title": "mrs",
-            "first": "patricia",
-            "last": "hale"
-          },
-          "nat": "GB"
-        },
-        {
-          "gender": "male",
-          "name": {
-            "title": "mr",
-            "first": "fernando",
-            "last": "cooper"
-          },
-          "nat": "US"
-        },
-        {
-          "gender": "female",
-          "name": {
-            "title": "mrs",
-            "first": "یسنا",
-            "last": "صدر"
+            "title": "Miss",
+            "first": "تینا",
+            "last": "جعفری"
           },
           "nat": "IR"
         },
         {
-          "gender": "male",
-          "name": {
-            "title": "mr",
-            "first": "eino",
-            "last": "tuomala"
-          },
-          "nat": "FI"
-        },
-        {
           "gender": "female",
           "name": {
-            "title": "ms",
-            "first": "gonca",
-            "last": "özkara"
+            "title": "Miss",
+            "first": "Deniz",
+            "last": "Çetin"
           },
           "nat": "TR"
         },
         {
           "gender": "male",
           "name": {
-            "title": "mr",
-            "first": "kyle",
-            "last": "castillo"
+            "title": "Mr",
+            "first": "Önal",
+            "last": "Numanoğlu"
           },
-          "nat": "US"
-        },
-        {
-          "gender": "female",
-          "name": {
-            "title": "miss",
-            "first": "olivia",
-            "last": "kumar"
-          },
-          "nat": "NZ"
+          "nat": "TR"
         },
         {
           "gender": "male",
           "name": {
-            "title": "monsieur",
-            "first": "raymond",
-            "last": "durand"
+            "title": "Mr",
+            "first": "Alfred",
+            "last": "Silva"
           },
-          "nat": "CH"
+          "nat": "IE"
         }
-    ]
+      ],
+      "info": {
+        "seed": "f6d6f992deb41934",
+        "results": 10,
+        "page": 1,
+        "version": "1.3"
+      }
+  }
+  if (req.url === "/data") {
+    res.end(JSON.stringify(data));
+  } else {
+    res.end("Hello World");
+  }
 })
 server.listen(port, () => {
-    console.log("Server is running on " + port);
+  console.log("Server is running on " + port);
 })
