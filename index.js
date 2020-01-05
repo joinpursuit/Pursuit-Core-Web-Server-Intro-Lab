e7da6f388c8400de266393a7407f4b5af8b6586f
Kevin Brutus <kevinbrutus@pursuit.org>
2020-01-05 17:45:28 -0500

    put data into divs

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let cards = document.querySelector(".card")
     axios.get("http://localhost:3000").then(res => {  
         let info = res.data[0]["results"][0]["results"]
-        // debugger
-        let div = document.createElement("div");
-        let person = document.createElement("ul")
+
         for(let i = 0; i < info.length; i++){
+            let div = document.createElement("div");
             let img = document.createElement("img")
+            let person = document.createElement("ul")
             let gender = document.createElement("li")
             let name = document.createElement("li")
             let nationality = document.createElement("li")
@@ -14,12 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
             gender.innerText = info[i].gender
             name.innerText = `${info[i].name.last},${info[i].name.first}`
             nationality.innerText = info[i].nat
-            person.appendChild(img)
             person.appendChild(gender)
             person.appendChild(name)
             person.appendChild(nationality)
+            div.appendChild(img)
+            div.appendChild(person)
+            cards.appendChild(div)
         }
-        div.appendChild(person)
-        cards.appendChild(div)
     })
 })
