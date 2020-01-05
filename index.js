b928fe1f54e7affcfd1e273327dfe45c210a9e91
maria liza <mariamartinez@pursuit.org>
2020-01-05 17:56:06 -0500

    work on css portion

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello World")
+    // console.log("Hello World")
+    let body = document.querySelector("body")
+    const populateDiv = () => {
+        axios.get("http://localhost:3000/data").then(res => {
+        //   debugger; 
+        res.data.results.forEach(el => {
+            let div = document.createElement("div");
+            let img = document.createElement("img");
+            img.src = "img_avatar2.png";
+            let h3 = document.createElement("h3")
+            h3.innerHTML = el.name.first
+            div.appendChild(img)
+            div.appendChild(h3)
+            body.appendChild(div)
+        })
+        })
+    }
+
+
+
+    populateDiv()
 })
