8f692b2f80938536cacaa0343f208228453b6bab
Wilsantos1975 <faustosantos@pursuit.org>
2020-01-05 17:27:12 -0500

    some css done

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let div = document.querySelector("div");
+    // let div = document.querySelector("div");
     axios.get("http://localhost:3000/").then(res => {
         res.data.forEach((el) => {
-            let ul = document.createElement("ul");
+            let div = document.createElement("div");
             let image = document.createElement("img");
             if(el.gender === "male") {
                 image.src="./img_avatar1male.png"
             } else {
                 image.src="./img_avatar2female.png"
             }
-            ul.appendChild(image)
+            div.appendChild(image)
             let title = document.createElement("li");
             title.innerText = el.name.title
             let name = document.createElement("li");
             name.innerText = el.name.first + " " + el.name.last
-            ul.appendChild(title);
-            ul.appendChild(name);
-            div.appendChild(ul)
+            div.appendChild(title);
+            div.appendChild(name);
+            document.body.appendChild(div)
         })
     })
 })
