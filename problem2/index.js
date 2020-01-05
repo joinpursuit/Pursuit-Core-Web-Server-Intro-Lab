80312a4de75d1cd096659aac7ae4d7f4df9d76db
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-01-05 17:37:58 -0500

    add awaitAnswer() recursive function

--- problem2/index.js
+++ problem2/index.js
@@ -1,57 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
     let container = document.querySelector("#container");
 
-    const answer = (correct) => {
+    const answer = (data, i) => {
         let form = document.querySelector("form");
         let select = document.querySelector("select");
         form.addEventListener("submit", (e) => {
             e.preventDefault();
-            if(select.value === correct) {
+            if(select.value === data[i].correct_answer) {
                 alert("CORRECT!");
             } else {
                 alert("YOU SUCK!");
             }
+            i += 1;
+            awaitAnswer(data, i);
         })
     }
 
-    axios.get("http://localhost:3000").then((res) => {
-        res.data.forEach(async (question) => {
-            container.innerHTML = "";
+    const awaitAnswer = (data, i = 0) => {
+        if(i === data.length) {
+            return;
+        }
+        let question = data[i];
+        container.innerHTML = "";
 
-            let p = document.createElement("p");
-            p.innerText = question.question;
-            container.appendChild(p);
+        let p = document.createElement("p");
+        p.innerText = question.question;
+        container.appendChild(p);
 
-            let select = document.createElement("select");
-            let answers = [];
-            answers.push(question.correct_answer);
+        let select = document.createElement("select");
+        let answers = [];
+        answers.push(question.correct_answer);
 
-            let incorrect = question.incorrect_answers;
-            incorrect.forEach(incAns => {
-                if(Math.floor(Math.random() * 2)) {
-                    answers.push(incAns);
-                } else {
-                    answers.unshift(incAns);
-                }
-            })
-            
-            answers.forEach(answer => {
-                let option = document.createElement("option");
-                option.innerText = answer;
-                option.value = answer;
-                select.appendChild(option);
-            })
-
-            let input = document.createElement("input");
-            input.type = "submit";
-
-            let form = document.createElement("form");
-            form.appendChild(select);
-            form.appendChild(input);
-
-            container.appendChild(form);
-
-            answer(question.correct_answer);
+        let incorrect = question.incorrect_answers;
+        incorrect.forEach(incAns => {
+            if(Math.floor(Math.random() * 2)) {
+                answers.push(incAns);
+            } else {
+                answers.unshift(incAns);
+            }
         })
-    })
+        
+        answers.forEach(answer => {
+            let option = document.createElement("option");
+            option.innerText = answer;
+            option.value = answer;
+            select.appendChild(option);
+        })
+
+        let input = document.createElement("input");
+        input.type = "submit";
+
+        let form = document.createElement("form");
+        form.appendChild(select);
+        form.appendChild(input);
+
+        container.appendChild(form);
+
+        answer(data, i);
+    }
+
+    axios.get("http://localhost:3000").then( async (res) => {
+        let data = res.data;
+        awaitAnswer(data);
+    }).catch(err => console.log(err))
 })
