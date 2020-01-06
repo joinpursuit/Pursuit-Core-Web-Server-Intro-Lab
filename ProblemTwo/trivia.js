e0045c0594a8bdc9855a3c2fe9db2e12efe5e5cd
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-06 13:37:57 -0500

    more javascript working on select and event listeners

--- ProblemTwo/trivia.js
+++ ProblemTwo/trivia.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    
+  questionSelect(triviaData)
 })
 const triviaData =async()=>{
     let url = "http://localhost:4001"
     let response = await axios.get(url)
-    
+    return response.data.results
 }
-let submitButton = document.querySelector("#submitButton")
+
+
+const questionSelect = (data)=>{
+    let submitButton = document.querySelector("#submitButton")
+    let p = document.querySelector("#p")
+    let select =document.querySelector("#select")
+    let wrongOptions = data.incorrect_answers
+    let correct = data.correct_answer
+    p.innerText = data.question
+    submitButton.innerText = "Submit"
+    submitButton.addEventListener("click"(event))
+}
