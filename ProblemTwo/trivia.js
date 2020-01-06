89e18f1aecf762b1bf629fdec00f648520f49a03
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-06 13:13:59 -0500

    addhtml and javascript

---
+++ ProblemTwo/trivia.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    
+})
+const triviaData =async()=>{
+    let url = "http://localhost:4001"
+    let response = await axios.get(url)
+    
+}
+let submitButton = document.querySelector("#submitButton")
