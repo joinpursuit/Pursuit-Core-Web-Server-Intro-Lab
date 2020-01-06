031b65fc8c74434c96176ed6db415ef6b29be194
John Jones Jr <jayjohnjones@gmail.com>
2020-01-06 13:06:16 -0500

    Part two of the questions

--- problem2/function.js
+++ problem2/function.js
@@ -3,29 +3,59 @@ document.addEventListener("DOMContentLoaded", () => {
     let select = document.querySelector("select")
     let btn = document.querySelector("button")
    let questionz = []
+   let cAns = []
+   let wAns = []
+   let count = 0;
+   let ans = "";
+   
+
+   const display = (count=0) => {
         axios.get(`http://localhost:3000/question`).then(res =>{
-            res.data.forEach(el => {
-                let option = document.createElement("option")
-                option.innerText = el.correct_answer
-                option.value =  el.correct_answer
-                option.innerText = el.incorrect_answers
-                option.value = el.incorrect_answers
-                select.appendChild(option)
-                questionz.push(el.question)
-                debugger
+            res.data.forEach((el,i) => {
+               
+             wAns.push(el.incorrect_answers)
+             cAns.push(el.correct_answer)
+            //  wAns.push(el.correct_answer)
+             questionz.push(el.question)
+                
             });
+            // debugger
+            p.innerText = questionz[count]
+            for(let i = 0; i < 3; i++){
+                let option = document.createElement("option")
+                option.innerText = wAns[count][i]
+                select.appendChild(option)
+            }
+            let option = document.createElement("option")
+                option.innerText = cAns[count]
+                select.appendChild(option)
         })
-
-
+    }    
 
         select.addEventListener("change",(e)=>{
-
+            ans = e.target.value    
+            debugger
         })
 
         btn.addEventListener("click",() => {
-
+            
+            
+            
+            if(ans === cAns[count]){
+                let ul = document.querySelector("ul")
+                let li = document.createElement("li")
+                li.innerText = "Correct!!"
+                ul.appendChild(li)
+                count++
+                display(count)
+                select.innerHTML = ""
+                debugger
+                } 
+                count++
+                display(count)
+                select.innerHTML = ""
         })
 
-
+        display(0);
 
 })
