a7af339c8db4b35d285c4bc8763ac9d6cf71a77a
Kevin Wong <kevinwong@pursuit.org>
2020-01-07 14:17:25 -0500

    write 2 functions getAnswer and checkAnswer, will need to be recursive to check one question at a time

--- problem2/index.js
+++ problem2/index.js
@@ -1,11 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-    axios.get("http://localhost:3000").then(res => {
-        let div = document.querySelector("#problem2");
-        res.data.forEach(el => {
-            let p = document.createElement("p");
-            p.innerText = el.question;
-            let select = document.createElement("select");
-            div.appendChild(p);
+    let div = document.querySelector("#problem2");
+
+    const checkAnswer = (data, i) => {
+        let form = document.querySelector("form");
+
+    }
+
+    const getAnswer = (data, i = 0) => {
+        let p = document.createElement("p");
+        p.innerText = data[i].question;
+        div.appendChild(p);
+
+        let form = document.createElement("form");
+        let select = document.createElement("select");
+        let answers = [];
+        
+        answers.push(data[i].correct_answer);
+
+        data[i].incorrect_answers.forEach(incorrectAnswer => {
+            Math.floor(Math.random() * 2) ? answers.push(incorrectAnswer) : answers.unshift(incorrectAnswer);
         })
+
+        answers.forEach(answer => {
+            let option = document.createElement('option');
+            option.innerText = answer;
+            select.appendChild(option);
+        })
+
+        let input = document.createElement('input');
+        input.type = "submit";
+
+        form.appendChild(select);
+        form.appendChild(input);
+
+        div.appendChild(form);
+        
+    }
+
+    axios.get("http://localhost:3000").then(res => {
+        let triviaData = res.data;
+        getAnswer(triviaData);
     })
 })
