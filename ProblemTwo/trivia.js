18bb610ce9f1b099d25ac54f359f940cde0032e7
Nilber Remon <nilberremon@gmail.com>
2020-01-06 20:00:53 -0500

    Problem 2 In Process

--- ProblemTwo/trivia.js
+++ ProblemTwo/trivia.js
@@ -1,20 +1,40 @@
-document.addEventListener("DOMContentLoaded",()=>{
-  questionSelect(triviaData)
+//document.addEventListener("DOMContentLoaded",()=>{
+//   let main = document.querySelector(".main")
+//       axios.get("http://localhost:4001").then(response=>{
+//           let data = response.data.results 
+//           debugger
+//           data.forEach(el=>{
+//             let question = document.querySelector("p")
+//             let button = document.querySelector("button")
+//             let answers = document.querySelector("select")
+//             question.innerText = el.results.question
+//             answers.innerText
+//           })
+//         })
+// })
+
+document.addEventListener("DOMContentLoaded", () => {
+  let submitButton = document.querySelector("#submitButton")
+  let p = document.querySelector("#p")
+  let select = document.querySelector("#select")
+  let quizData
+
+
+  const questionSelect = (data) => {
+    // let wrongOptions = data.incorrect_answers
+    // let correct = data.correct_answer
+    // p.innerText = data.question
+    // submitButton.innerText = "Submit"
+    // submitButton.addEventListener("click"(event))
+  }
+  
+  const jsonData = async () => {
+    let response = await axios.get("http://localhost:4001")
+    quizData = response.data.results
+    debugger
+  }
+  
+  jsonData()
+  questionSelect(quizData)
+  
 })
-const triviaData =async()=>{
-    let url = "http://localhost:4001"
-    let response = await axios.get(url)
-    return response.data.results
-}
-
-
-const questionSelect = (data)=>{
-    let submitButton = document.querySelector("#submitButton")
-    let p = document.querySelector("#p")
-    let select =document.querySelector("#select")
-    let wrongOptions = data.incorrect_answers
-    let correct = data.correct_answer
-    p.innerText = data.question
-    submitButton.innerText = "Submit"
-    submitButton.addEventListener("click"(event))
-}
