c3c533a3583b4ad96b7c4bcde2fd4aa521ac3912
Wilsantos1975 <faustosantos@pursuit.org>
2020-01-06 20:02:12 -0500

    question 2 unresolved

--- indexP2.js
+++ indexP2.js
@@ -1,11 +1,39 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-   axios.get("http://localhost:3000").then(res=>{
+    let questionPool = [];
+    let displayquestion = [];
+    let answer=[]
+    let num=0
+    axios.get("http://localhost:3000").then(res=>{
+        let questionPool = res.data;
+        displayquestion=questionPool[num];
+        answer=displayquestion.incorrect_answers
+        answer.push(displayquestion.correct_answer)
+        shuffle(answer);
+        
+        let p=document.querySelector("#question"); 
+        p.innerText = displayquestion.question;
+
+        let select = document.querySelector("select")
+        answer.forEach(el=>{
+           let option = document.createElement("option");
+           option.innerText = el;
+        select.appendChild(option)
+       })
+   })
+   let btn =document.querySelector("#button")
+   btn.addEventListener("click", (e) => {
        debugger
-    res.data.forEach(el=>{
-        let p=document.querySelector("#question");
-        p.innerText=el.question;
-    })
    })
 
 
+
+
 })
+
+//    num++
+
+
+
+const shuffle =(array)=> {
+    array.sort(() => Math.random() - 0.5);
+  }
