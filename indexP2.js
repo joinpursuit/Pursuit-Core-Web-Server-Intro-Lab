d7b849a76660e44ba241f81b08119a51f1824f69
your name here <your_email_here>
2020-01-07 00:47:06 -0500

    finished Lab

--- indexP2.js
+++ indexP2.js
@@ -1,39 +1,51 @@
+let questionPool = [];
+let displayquestion = [];
+let answer=[];
+let num=0;
+
 document.addEventListener("DOMContentLoaded", ()=>{
-    let questionPool = [];
-    let displayquestion = [];
-    let answer=[]
-    let num=0
     axios.get("http://localhost:3000").then(res=>{
-        let questionPool = res.data;
-        displayquestion=questionPool[num];
-        answer=displayquestion.incorrect_answers
-        answer.push(displayquestion.correct_answer)
-        shuffle(answer);
-        
-        let p=document.querySelector("#question"); 
-        p.innerText = displayquestion.question;
+        questionPool = res.data;
+        shuffle(questionPool);
+        getQuestion();
+        showonHTML()
+    })
 
-        let select = document.querySelector("select")
-        answer.forEach(el=>{
-           let option = document.createElement("option");
-           option.innerText = el;
-        select.appendChild(option)
-       })
-   })
-   let btn =document.querySelector("#button")
-   btn.addEventListener("click", (e) => {
-       debugger
-   })
-
-
-
-
+    let btn =document.querySelector("button")
+    btn.addEventListener("click",event=>{
+        let sel =document.querySelector("select")
+        if(sel.selectedOptions[0].value===displayquestion.correct_answer){
+        alert("you are right"),
+           getQuestion();
+           showonHTML()
+        }else{
+            alert("your are wrong, please select different answer")
+        }
+    })
+    
 })
 
-//    num++
+const getQuestion=()=>{
+    displayquestion=questionPool.pop();
+    answer=[];
+    answer=displayquestion.incorrect_answers;
+    answer.push(displayquestion.correct_answer)
+    shuffle(answer);
+}
 
+const showonHTML=()=>{
+    let p=document.querySelector("#question"); 
+    p.innerText = displayquestion.question;
+    let select = document.querySelector("select")
+    select.innerHTML=""
 
+    answer.forEach(el=>{
+        let option = document.createElement("option");
+        option.innerText = el;
+        select.appendChild(option)
+    })
+}
 
-const shuffle =(array)=> {
-    array.sort(() => Math.random() - 0.5);
-  }
+    const shuffle =(array)=> {
+        array.sort(() => Math.random() - 0.5);
+      }
