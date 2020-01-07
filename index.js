d673eb5f6bb4dc53bed8338a976207b5beb3049c
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-06 19:34:18 -0500

    add divs and cards

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     axios.get("http://localhost:3000/").then(res => {
     res.data.forEach(person => {
         let container = document.querySelector('#container')
-        let personDiv = document.createElement('div')
+        let male_card = document.querySelector('.div')
+        let female_card = document.querySelector('.div')
          let personName = person.name.first + " " + person.name.last + " " + person.gender
         let h3 = document.createElement("h3")
          let li = document.createElement("li")
-        
+
         h3.innerText = personName
         debugger
         document.body.appendChild(h3)
-        
-        
-    })   
+
+
+    })
 })
 
 })
