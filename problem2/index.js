de5ef29f49c38b0816f24ccd60b1f3a2f3e97624
Kevin Wong <kevinwong@pursuit.org>
2020-01-07 14:39:53 -0500

    add shuffle to answers array, add recursive, finish checkAnswer function, complete lab

--- problem2/index.js
+++ problem2/index.js
@@ -1,15 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     let div = document.querySelector("#problem2");
 
+    const shuffleArr = (arr) => {
+        arr.sort(() => Math.random() - 0.5);
+    }
+
     const checkAnswer = (data, i) => {
         let form = document.querySelector("form");
         let select = document.querySelector("select");
-        form.addEventListener("submit", () => {
-            
+        form.addEventListener("submit", (e) => {
+            select.value === data[i].correct_answer ? alert("You're correct!") : alert("You're wrong!");   
+            i++;
+            getAnswer(data, i);
         })
     }
 
     const getAnswer = (data, i = 0) => {
+        if(i === data.length) return;
+        div.innerHTML = "";
+
         let p = document.createElement("p");
         p.innerText = data[i].question;
         div.appendChild(p);
@@ -19,10 +28,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let answers = [];
         
         answers.push(data[i].correct_answer);
-
         data[i].incorrect_answers.forEach(incorrectAnswer => {
-            Math.floor(Math.random() * 2) ? answers.push(incorrectAnswer) : answers.unshift(incorrectAnswer);
+            answers.push(incorrectAnswer);
         })
+        shuffleArr(answers);
 
         answers.forEach(answer => {
             let option = document.createElement('option');
@@ -38,6 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         div.appendChild(form);
 
+        checkAnswer(data, i);
     }
 
     axios.get("http://localhost:3000").then(res => {
